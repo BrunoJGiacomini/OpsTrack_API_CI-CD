1cc933c33ee81132febe21eb072a164891f7ae10
BrunoJGiacomini <brunojunigiacomini@gmail.com>
2026-08-17 10:15:54 -0300

    feat: Adding new route (status)

---
+++ anotacoes/aula-02.md
@@ -0,0 +1,3 @@
+#Aula 2 - Convetional Commits
+
+git feat, fix, docs, refactor, test, style, chore, BREAKING CHANGE
--- app.py
+++ app.py
@@ -4,7 +4,15 @@ app = Flask(__name__)
 
 @app.route("/route", methods=["GET"])
 def route():
-    return "Vai Corinthians"
+    return "Vai Corinthians!"
+
+@app.route("/status")
+def status():
+    return {
+        "status": "ok",
+        "version": "1.0.0",
+    }
+
 
 if __name__ == "__main__":
     app.run(debug=True)
